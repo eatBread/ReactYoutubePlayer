9f6951f9bc8a7e9bb06f55cf9f22cbe3ef18aa7b
eatBread <junoleetravian@gmail.com>
2018-01-24 15:47:43 -0800

    refactor index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyA9IT5OUEczuwrVsRjQpAFCq_tWmuEcOuw'
-// Create a new component. This component should produce some Html
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: []
+		};
+
+		YTSearch({key: API_KEY, term: 'surfboard'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
+
 // Take this component's generated HTML and put it on the page
 ReactDOM.render(
 	<App />,
