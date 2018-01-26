31ce8dfd37f973a81da0e0120614574438a84200
eatBread <junoleetravian@gmail.com>
2018-01-26 10:56:55 -0800

    video list content

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { term: 'I love Vlog' };
+		this.state = { term: '' };
 		this.onInputChange = this.onInputChange.bind(this);
 	}
 
